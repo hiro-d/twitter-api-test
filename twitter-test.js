2a666fac4f0be30807623c18f4921ca09afe730f
hiroki deguchi <shimamura000@gmail.com>
2017-05-09 00:48:18 +0900

    apiをいじる簡易スクリプト

---
+++ twitter-test.js
@@ -0,0 +1,18 @@
+var twitter = require('twitter');
+var config = require('config');
+
+var twitter_config = config.get('Twitter');
+var client = new twitter({
+  consumer_key: twitter_config.get('consumer_key'),
+  consumer_secret: twitter_config.get('consumer_secret'),
+  access_token_key: twitter_config.get('access_token_key'),
+  access_token_secret: twitter_config.get('access_token_secret')
+});
+
+var params = {q: "Lv100 セレスト", lang: 'ja', locale: 'ja', result_type: 'recent'};
+
+client.get('search/tweets', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
